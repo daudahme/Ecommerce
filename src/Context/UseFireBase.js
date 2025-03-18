287de1473a7430689ef58588b5882a45a64fa664
daudahme <hassam.mughal2001@gmail.com>
2025-03-18 16:49:04 +0500

    firebaseupload

---
+++ src/Context/UseFireBase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYrALfW71f5sXUBwglLGhcyv8mXufdH6s",
+  authDomain: "ecommerce-4d66f.firebaseapp.com",
+  projectId: "ecommerce-4d66f",
+  storageBucket: "ecommerce-4d66f.firebasestorage.app",
+  messagingSenderId: "359786962053",
+  appId: "1:359786962053:web:7c1c3ecd3241028796ddec",
+  measurementId: "G-XKL4GT3HTN"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app);
+
+export const signinwithEmail =async(email,password)=>{
+    await signInWithEmailAndPassword(auth,email,password)
+    
+}
+
+export const signout =async()=>{
+  await  signOut(auth)
+
+
+}
